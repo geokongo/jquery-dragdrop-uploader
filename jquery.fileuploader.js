dbb763eb567abd4c4c1301319b796ef343c63657
geoffreyoliver <geoffreyokongo2@gmail.com>
2016-07-05 10:26:05 +0300

    Creating function to calculate human readable file size

--- jquery.fileuploader.js
+++ jquery.fileuploader.js
@@ -223,7 +223,7 @@
 				var anyFile = document.createElement("p");
 				anyFile.style.padding = "4px";
 				var fileName = (file.name.length >= 20) ? "..."+file.name.substr(file.name.length - 15) : file.name;
-				anyFile.innerHTML =  fileName+ "<br>" + file.size + " bytes";
+				anyFile.innerHTML =  fileName+ "<br>" + humanReadableFileSize(file.size);
 				itemContainer.appendChild(anyFile);
 
 				//append to the preview div
@@ -277,6 +277,35 @@
 			}
 		}
 
+		/**
+		 * This method returns the file size in human readable format
+		 * @param Integer bytes The file size in raw bytes
+		 * @return String The readable string format of the file size
+		 */
+		function humanReadableFileSize(bytes, si = true) {
+
+		    var thresh = si ? 1000 : 1024;
+
+		    if(Math.abs(bytes) < thresh) {
+
+		        return bytes + ' B';
+
+		    }
+
+		    var units = si
+		        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+		        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+		    var u = -1;
+
+		    do {
+
+		        bytes /= thresh;
+		        ++u;
+
+		    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+
+		    return bytes.toFixed(1)+' '+units[u];
+		}
 	};
 
 }(jQuery));
